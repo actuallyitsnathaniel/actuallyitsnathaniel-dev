f8b74221dd163935c53881aefd6e1763187ace49
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2021-09-09 23:27:33 -0700

    successful real-time api calls

--- src/Components/GitRepoCards/index.js
+++ src/Components/GitRepoCards/index.js
@@ -1,25 +1,59 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 import './GitRepoCardSet.css';
-import { GitRepoCard } from './GitRepoCard';
 
 import githubLogo from '../../assets/images/git-repos-icon.png';
 
 
 // Dynamically propagated based on number of repositories
-class GitRepoCardSet extends Component {
+function GitRepoCard() {
 
-    // Placeholder until all is dynamically allocated
-    render() {
+    // fetch HERE
+    const publicKey = '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';
+    const [repos, setRepos] = useState([]);
+    
+    // Fix GetData
+    const GetData=()=>
+        useEffect(()=>{
+            fetch(`https://api.github.com/users/actuallyitsnathaniel/repos`, {
+            headers: {
+                //'Authorization': `token ${publicKey}`,
+                'Content-Type': 'application/json',
+            }
+        }).then((response)=> {
+            if (response.ok) return response.json();
+        }).then((json=>{
+            setRepos(json);
+        }))
+        .catch((err) => {
+            console.log(err);
+            return err;
+        });
+        
+        }, [])
 
-        return(
-            <div>
+        
+        GetData();
+
+        // THIS LOG PROVES REPOS WORKS  
+        // console.log(repos);      
+        
+    return(
+        <div>
             <img src={githubLogo} alt="GitHub" className="github-text github-logo"/>
-                <GitRepoCard/>
-            </div>
-      
+            <div className="git-repo-card-set">{
+                repos.map((details, i) => {
+                return (
+                    <a className="git-repo-card" href={details.html_url} key={details.id}>
+                        <div className="git-repo-card-title" >{details.name}</div>
+                        <div className="git-repo-card-desc" >{details.description}</div>
+                        <div className="git-repo-card-codeType" >{details.language}</div>
+                    </a>
+                    )
+            })}</div>
+        </div>
     );
-    }
+        
 }
 
-export default GitRepoCardSet;
+export default GitRepoCard;
