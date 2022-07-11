006eea556712494d027ac01522044693e4a2b75b
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2022-07-11 15:48:18 -0700

    heavy fixes and icon replacements

--- src/App/index.js
+++ src/App/index.js
@@ -1,69 +1,80 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import EducationAndSkills from '../Pages/EducationAndSkills';
+import EducationAndSkills from "../Pages/EducationAndSkills";
 import AboutMe from "../Pages/AboutMe";
-import ContactMe from '../Pages/ContactMe';
-import Projects from '../Pages/Projects';
+import ContactMe from "../Pages/ContactMe";
+import Projects from "../Pages/Projects";
 
-import '../fonts/vcr_osd_mono.ttf';
-import backgroundVideo from '../assets/videos/node_background.mp4';
-import profilePic from '../assets/images/profile_pic.png';
-import resume from '../assets/files/resume.pdf';
-
+import "../fonts/vcr_osd_mono.ttf";
+import backgroundVideo from "../assets/videos/node_background.mp4";
+import profilePic from "../assets/images/profile_pic.png";
+import resume from "../assets/files/resume.pdf";
 
 // for social links
-import {FaGithub, FaLinkedin} from 'react-icons/fa';
-import {AiOutlineMail} from 'react-icons/ai';
-import resumeIcon from '../assets/images/resume-icon.svg';
-
+import linkedin from "../assets/images/linkedin-svg.svg";
+import github from "../assets/images/github-svg.svg";
+import mail from "../assets/images/mail-svg.svg";
+import resumeIcon from "../assets/images/resume-icon.svg";
 
 function App() {
   return (
-    
     <div className="App">
-      <video autoPlay loop muted id='video'>
-        <source src={backgroundVideo} type='video/mp4'/>
+      <video autoPlay loop muted id="video">
+        <source src={backgroundVideo} type="video/mp4" />
         Sorry, your browser does not support HTML5 video.
       </video>
-    {/** STATIC EXTERNAL LINKS*/}
+      {/** STATIC EXTERNAL LINKS*/}
       <div className="links-div">
-        <a href={"mailto:nathanielrbowman@gmail.com"}><AiOutlineMail className="link" alt="email" /></a>
-        <a href={"https://linkedin.com/in/actuallyitsnathaniel"}><FaLinkedin className="link" alt="linkedin" /></a>
-        <a href={"https://github.com/actuallyitsnathaniel"}> <FaGithub className="link" alt="github" /> </a>
-        <a href={resume} rel="noopener noreferrer" type="application/pdf"  download="nathaniel-bowman_resume" >
-        <img src={resumeIcon} className="link" alt="resume" /></a>
+        <a href={"mailto:nathanielrbowman@gmail.com"}>
+          <img src={mail} className="link" alt="email" />
+        </a>
+        <a href={"https://linkedin.com/in/actuallyitsnathaniel"}>
+          <img src={linkedin} className="link" alt="linkedin" />
+        </a>
+        <a href={"https://github.com/actuallyitsnathaniel"}>
+          {" "}
+          <img src={github} className="link" alt="github" />{" "}
+        </a>
+        <a
+          href={resume}
+          rel="noopener noreferrer"
+          type="application/pdf"
+          download="nathaniel-bowman_resume"
+        >
+          <img src={resumeIcon} className="link" alt="resume" />
+        </a>
       </div>
 
-
-      
       {/** TOP DIV */}
       <header className="Top-header">
-      <img src={profilePic} alt="profile_pic" className="profile-pic"/>
-      NATHANIEL BOWMAN
-      <p className="bio">B.S. Computer Science, looking for work! <br/>
-      I'm a motivated software engineer who reads the documentation and checks the features before an update.
-      I think critically to look for creative solutions. I love collaboration, problem-solving, and Agile Development.
-      
-      </p>
-      <div className="arrow">
-        ⬇
-      </div> 
-      <p/>
+        <img src={profilePic} alt="profile_pic" className="profile-pic" />
+        NATHANIEL BOWMAN
+        <p className="bio">
+          B.S. Computer Science, looking for work! <br />
+          I'm a motivated software engineer who reads the documentation and
+          checks the features before an update. I think critically to look for
+          creative solutions. I love collaboration, problem-solving, and Agile
+          Development.
+        </p>
+        <div className="arrow">⬇</div>
+        <p />
       </header>
-          
-      <EducationAndSkills/>
-      <div className="arrow">
-      ⬇
-      </div>
-      <p/>
-      <p/>
+
+      <EducationAndSkills />
+      <div className="arrow">⬇</div>
+      <p />
+      <p />
       <Projects />
 
-      <AboutMe className="about-me"/>
+      <AboutMe className="about-me" />
       <ContactMe />
 
-      <footer className="footer"> © NATHANIEL BOWMAN 2021. Built exclusively with React, HTML, and CSS. Deployed on Bluehost®.</footer>
+      <footer className="footer">
+        {" "}
+        © NATHANIEL BOWMAN 2021. Built exclusively with React, HTML, and CSS.
+        Deployed on Bluehost®.
+      </footer>
     </div>
   );
 }
