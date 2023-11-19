8175574e95518738fab79ad1f93c49f801f6bad1
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-11-19 14:21:05 -0800

    updated resume, fixed skills items on mobile

--- src/Components/skill-item/index.jsx
+++ src/Components/skill-item/index.jsx
@@ -4,7 +4,7 @@ export const SkillItem = ({ href, image, alt, color, classNames }) => {
       <img
         src={image}
         alt={alt}
-        className={`transition-all duration-100 fill-[${color}] hover:fill-gray-400 contrast-[70%] hover:contrast-100 brightness-150 hover:brightness-100 saturate-0 hover:saturate-100 w-24 p-0.5 ${classNames}`}
+        className={`transition-all duration-100 fill-[${color}] hover:fill-gray-400 md:contrast-[70%] hover:contrast-100 hover:brightness-110 md:saturate-0 hover:saturate-100 w-24 p-0.5 ${classNames}`}
       />
     </a>
   );
