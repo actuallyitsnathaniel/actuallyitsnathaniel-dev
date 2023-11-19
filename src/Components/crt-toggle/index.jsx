7ae6f7ef68f955d8b5140c95f77820c9a37a7bad
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-11-18 20:07:44 -0800

    trying to remove roundedness

--- src/Components/crt-toggle/index.jsx
+++ src/Components/crt-toggle/index.jsx
@@ -4,7 +4,7 @@ export const CRTToggle = ({ HandleCRT }) => {
       <input
         type="checkbox"
         onClick={HandleCRT}
-        className="appearance-none h-4 w-4 m-2 outline-3 outline-dashed outline-offset-2 bg-white checked:bg-transparent"
+        className="appearance-none h-4 w-4 m-2 outline-3 outline-dashed outline-offset-2 bg-white checked:bg-transparent rounded-none"
       />
       <p className="flex uppercase text-xl align-middle m-1">CRT TOGGLE</p>
     </span>
