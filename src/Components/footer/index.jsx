0f968fcd0fbbedb26da504462e496e4650ed323d
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-11-18 19:53:30 -0800

    custom scrollbar

--- src/Components/footer/index.jsx
+++ src/Components/footer/index.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="flex flex-col items-center min-h-[3vh] text-xs text-center p-5">
+    <footer className="flex flex-col min-h-[3vh] max-w-3xl mx-auto text-sm text-center p-5">
       © NATHANIEL BOWMAN {new Date().getFullYear()}. Built exclusively with
       Vite, React, and TailwindCSS. Deployed on Vercel.
     </footer>
