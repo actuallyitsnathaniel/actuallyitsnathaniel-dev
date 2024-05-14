03b990f62c2b38d3154cd8faeb8340369e394ead
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-05-14 12:27:19 -0700

    updated skills

--- src/Pages/EducationAndSkills.jsx
+++ src/Pages/EducationAndSkills.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
+import apu from "/src/assets/images/apu_white.svg";
+import aws from "/src/assets/images/skills/aws-logo.svg";
+import css3_svg from "/src/assets/images/skills/css-svg.svg";
+import docker_svg from "/src/assets/images/skills/docker-logo-blue.svg";
+import firebase from "/src/assets/images/skills/firebase-logomark.svg";
+import gcp from "/src/assets/images/skills/google-cloud-seeklogo.com.svg";
 import github_svg from "/src/assets/images/skills/github_square_icon.svg";
 import html_svg from "/src/assets/images/skills/html-svg.svg";
-import css3_svg from "/src/assets/images/skills/css-svg.svg";
-import mongodb_svg from "/src/assets/images/skills/mongodb-svg.svg";
-import react_svg from "/src/assets/images/skills/react-svg.svg";
-import js_svg from "/src/assets/images/skills/js-svg.svg";
-import ts_svg from "/src/assets/images/skills/typescript-logo-svg.svg";
-import gcp from "/src/assets/images/skills/google-cloud-seeklogo.com.svg";
-import aws from "/src/assets/images/skills/aws-logo.svg";
-import firebase from "/src/assets/images/skills/firebase-logomark.svg";
 import java_svg from "/src/assets/images/skills/java.svg";
-import apu from "/src/assets/images/apu_white.svg";
+import jenkins_svg from "/src/assets/images/skills/jenkins_logo.svg";
+import js_svg from "/src/assets/images/skills/js-svg.svg";
+import mongodb_svg from "/src/assets/images/skills/mongodb-svg.svg";
 import mysql from "/src/assets/images/skills/logo-mysql.svg";
+import postgresql_logo from "/src/assets/images/skills/postgresql-logo.svg";
+import rails_svg from "/src/assets/images/skills/ruby-on-rails-logo.svg";
+import react_svg from "/src/assets/images/skills/react-svg.svg";
+import splunk_svg from "/src/assets/images/skills/splunk-logo.svg";
+import tailwindcss_svg from "/src/assets/images/skills/tailwindcss-logo.svg";
+import ts_svg from "/src/assets/images/skills/typescript-logo-svg.svg";
+import vercel_svg from "/src/assets/images/skills/vercel-logotype-light.svg";
 
 import { SkillItem } from "/src/Components/skill-item";
 
@@ -35,13 +42,44 @@ const SkillsAndExperience = () => {
           id="skill-items"
           className="flex flex-wrap items-center justify-center py-10"
         >
-          {/* TODO: add RubyOnRails, PostgreSQL, Vercel, Jenkins, Docker, Splunk */}
+          {/* TODO: implement an interactable 3D "skill-sphere"
+          --- this will then require that i include babylon.js in my skills 😎  */}
           <SkillItem
-            href={"https://www.java.com/en/"}
-            image={java_svg}
-            alt={"java"}
-            color={"white"}
-            classNames={"-translate-y-6"}
+            href="https://aws.amazon.com/"
+            image={aws}
+            alt={"aws"}
+            classNames={"hover:brightness-110"}
+          />
+          <SkillItem
+            href={"https://developer.mozilla.org/en-US/docs/Web/CSS"}
+            image={css3_svg}
+            alt={"css3"}
+            color={"007df2"}
+          />
+          <SkillItem
+            image={docker_svg}
+            href={"https://www.docker.com/"}
+            alt={"docker"}
+            classNames={"scale-125"}
+          />
+          <SkillItem
+            href={"https://firebase.google.com"}
+            image={firebase}
+            alt={"firebase"}
+            classNames={"scale-90"}
+          />
+          <SkillItem
+            href="https://cloud.google.com"
+            image={gcp}
+            alt={"google-cloud-platform"}
+            classNames="scale-110"
+          />
+          <SkillItem
+            href={"https://www.github.com"}
+            image={github_svg}
+            alt={"github"}
+            color={"DDDEEE"}
+            classNames={"rounded-xl scale-95"}
           />
           <SkillItem
             href="https://developer.mozilla.org/en-US/docs/Web/HTML"
@@ -50,10 +88,11 @@ const SkillsAndExperience = () => {
             color={"eb8300"}
           />
           <SkillItem
-            href={"https://developer.mozilla.org/en-US/docs/Web/CSS"}
-            image={css3_svg}
-            alt={"css3"}
-            color={"007df2"}
+            href={"https://www.java.com/en/"}
+            image={java_svg}
+            alt={"java"}
+            color={"white"}
+            classNames={"scale-75"}
           />
           <SkillItem
             href={"https://www.javascript.com"}
@@ -63,17 +102,28 @@ const SkillsAndExperience = () => {
             classNames={"scale-95 px-1.5"}
           />
           <SkillItem
+            image={jenkins_svg}
+            href={"https://www.jenkins.io/"}
+            alt={"jenkins"}
+            classNames={"scale-90"}
+          />
+          <SkillItem
+            href={"https://www.mysql.com/"}
+            image={mysql}
+            alt={"mySQL"}
+            classNames={"scale-110"}
+          />
+          <SkillItem
             href={"https://mongodb.com"}
             image={mongodb_svg}
             alt={"mongo-db"}
             color={"0d9e2c"}
           />
           <SkillItem
-            href={"https://www.github.com"}
-            image={github_svg}
-            alt={"github"}
-            color={"DDDEEE"}
-            classNames={"object-contain rounded-xl"}
+            href={"https://www.postgresql.org/"}
+            image={postgresql_logo}
+            alt={"postgresql"}
+            classNames={"scale-150"}
           />
           <SkillItem
             href="https://reactjs.org"
@@ -82,33 +132,34 @@ const SkillsAndExperience = () => {
             color={"50CAEA"}
           />
           <SkillItem
+            href={"https://rubyonrails.org/"}
+            image={rails_svg}
+            alt={"rails"}
+            classNames={"scale-110"}
+          />
+          <SkillItem
+            href={"https://www.splunk.com/"}
+            image={splunk_svg}
+            alt={"splunk"}
+            classNames={"scale-125"}
+          />
+          <SkillItem
+            href={"https://tailwindcss.com/"}
+            alt={"tailwindcss"}
+            image={tailwindcss_svg}
+          />
+          <SkillItem
             href="https://www.typescriptlang.org/"
             image={ts_svg}
             alt={"typescript"}
             color={"2495ff"}
-          />
-          <SkillItem
-            href="https://cloud.google.com"
-            image={gcp}
-            alt={"google-cloud-platform"}
-            classNames="scale-110"
-          />
-          <SkillItem
-            href={"https://firebase.google.com"}
-            image={firebase}
-            alt={"firebase"}
             classNames={"scale-90"}
           />
           <SkillItem
-            href="https://aws.amazon.com/"
-            image={aws}
-            alt={"aws"}
-            classNames={"hover:brightness-110"}
-          />
-          <SkillItem
-            href={"https://www.mysql.com/"}
-            image={mysql}
-            alt={"mySQL"}
+            href={"https://vercel.com/"}
+            image={vercel_svg}
+            alt={"vercel"}
+            classNames={"scale-125"}
           />
         </span>
       </div>
