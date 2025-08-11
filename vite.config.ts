598f823373e7a2353e1fb858278870b3e0892df0
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2025-08-10 17:21:12 -0700

    replaced postcss version of Tailwind with vite version

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -17,6 +18,7 @@ export default defineConfig(({ mode }) => {
     // depending on your application, base can also be "/"
     base: "",
     plugins: [
+      tailwindcss(),
       react(),
       svgr(),
       ViteImageOptimizer({
